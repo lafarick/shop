5bf2f2db109704c7654510a32db399f5d431cb2e
LafaRick <anastasia.k.developer.com>
2022-07-12 16:24:32 +0300

    Added json.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"shop/choice"
 	"shop/db"
+	"shop/models"
 	"shop/serverHello"
 )
 
@@ -11,6 +14,21 @@ func main() {
 	database, err := db.Connect()
 	if err != nil {
 		panic(err)
+	}
+
+	jsonString := `[{"first_word": "Say", "second_word": "Hello", "third_word": "World"}]`
+
+	helloWorld := []models.HelloWorld{}
+
+	err = json.Unmarshal([]byte(jsonString), &helloWorld)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, h := range helloWorld {
+		fmt.Println(h.FirstWord, h.SecondWord, h.ThirdWord)
+
 	}
 
 	/*err = db.CreateTableSellers(database)
---
+++ models/helloworld.go
@@ -0,0 +1,7 @@
+package models
+
+type HelloWorld struct {
+	FirstWord  string `json:"first_word"`
+	SecondWord string `json:"second_word"`
+	ThirdWord  string `json:"third_word"`
+}
